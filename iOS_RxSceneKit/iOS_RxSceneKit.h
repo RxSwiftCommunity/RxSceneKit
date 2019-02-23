e5e7f0a50a3d15ac30e7399378653d4e8b2c7552
Maxim Volgin <maxim.volgin@me.com>
2019-02-23 08:28:02 +0100

    macOS target

---
+++ iOS_RxSceneKit/iOS_RxSceneKit.h
@@ -0,0 +1,19 @@
+//
+//  iOS_RxSceneKit.h
+//  iOS_RxSceneKit
+//
+//  Created by Maxim Volgin on 23/02/2019.
+//  Copyright © 2019 Maxim Volgin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for iOS_RxSceneKit.
+FOUNDATION_EXPORT double iOS_RxSceneKitVersionNumber;
+
+//! Project version string for iOS_RxSceneKit.
+FOUNDATION_EXPORT const unsigned char iOS_RxSceneKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iOS_RxSceneKit/PublicHeader.h>
+
+
---
+++ macOS_RxSceneKit/macOS_RxSceneKit.h
@@ -0,0 +1,19 @@
+//
+//  macOS_RxSceneKit.h
+//  macOS_RxSceneKit
+//
+//  Created by Maxim Volgin on 23/02/2019.
+//  Copyright © 2019 Maxim Volgin. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for macOS_RxSceneKit.
+FOUNDATION_EXPORT double macOS_RxSceneKitVersionNumber;
+
+//! Project version string for macOS_RxSceneKit.
+FOUNDATION_EXPORT const unsigned char macOS_RxSceneKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <macOS_RxSceneKit/PublicHeader.h>
+
+
